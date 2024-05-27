fd637af15d8297252e2f0b08208bb19095c2f011
Gamze <101417422+Gamzekasyol@users.noreply.github.com>
2024-05-28 01:54:00 +0300

    not hesaplama
    Kullanıcının girdiği çan eğrisine göre yine kullanıcının girdiği sınav notlarını hesaplayarak geçip geçmediğinizi hesaplayan uygulama.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace NotHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSinav1 = new System.Windows.Forms.TextBox();
+            this.txtSinav2 = new System.Windows.Forms.TextBox();
+            this.txtSinav3 = new System.Windows.Forms.TextBox();
+            this.txtsonuc = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.txtgecme = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sınav1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sınav 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sınav 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(518, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sonuç";
+            // 
+            // txtSinav1
+            // 
+            this.txtSinav1.Location = new System.Drawing.Point(170, 48);
+            this.txtSinav1.Name = "txtSinav1";
+            this.txtSinav1.Size = new System.Drawing.Size(100, 22);
+            this.txtSinav1.TabIndex = 4;
+            // 
+            // txtSinav2
+            // 
+            this.txtSinav2.Location = new System.Drawing.Point(170, 97);
+            this.txtSinav2.Name = "txtSinav2";
+            this.txtSinav2.Size = new System.Drawing.Size(100, 22);
+            this.txtSinav2.TabIndex = 5;
+            // 
+            // txtSinav3
+            // 
+            this.txtSinav3.Location = new System.Drawing.Point(170, 140);
+            this.txtSinav3.Name = "txtSinav3";
+            this.txtSinav3.Size = new System.Drawing.Size(100, 22);
+            this.txtSinav3.TabIndex = 6;
+            // 
+            // txtsonuc
+            // 
+            this.txtsonuc.Location = new System.Drawing.Point(498, 157);
+            this.txtsonuc.Name = "txtsonuc";
+            this.txtsonuc.Size = new System.Drawing.Size(100, 22);
+            this.txtsonuc.TabIndex = 7;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(332, 54);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(91, 42);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(332, 120);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(91, 36);
+            this.btnTemizle.TabIndex = 9;
+            this.btnTemizle.Text = "TEMİZLE";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // txtgecme
+            // 
+            this.txtgecme.Location = new System.Drawing.Point(489, 64);
+            this.txtgecme.Name = "txtgecme";
+            this.txtgecme.Size = new System.Drawing.Size(100, 22);
+            this.txtgecme.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(495, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Geçme Notu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(728, 414);
+            this.Controls.Add(this.txtgecme);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtsonuc);
+            this.Controls.Add(this.txtSinav3);
+            this.Controls.Add(this.txtSinav2);
+            this.Controls.Add(this.txtSinav1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Not Hesaplama ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSinav1;
+        private System.Windows.Forms.TextBox txtSinav2;
+        private System.Windows.Forms.TextBox txtSinav3;
+        private System.Windows.Forms.TextBox txtsonuc;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.TextBox txtgecme;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3, sonuc, gecme_notu;
+            sayi1 = Convert.ToInt32(txtSinav1.Text);
+            sayi2 = Convert.ToInt32(txtSinav2.Text);
+            sayi3 = Convert.ToInt32(txtSinav3.Text);
+            gecme_notu = Convert.ToInt32(txtgecme.Text);
+
+            sonuc = (sayi1 + sayi2 + sayi3) / 3;
+
+            if (sonuc < gecme_notu)
+            {
+                txtsonuc.Text = sonuc.ToString();
+                txtsonuc.BackColor = Color.Red;
+                txtsonuc.ForeColor = Color.White;
+            }
+            else
+            {
+                txtsonuc.Text = sonuc.ToString();
+                txtsonuc.BackColor = Color.Green;
+                txtsonuc.ForeColor = Color.White;
+
+            }
+
+            
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtSinav1.Text = "";
+            txtSinav2.Text = "";
+            txtSinav3.Text = "";
+            txtsonuc.Text = "";
+            txtsonuc.BackColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
